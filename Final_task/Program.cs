bd73286f9dfeb9647e7582ca46314d8f286b9848
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-03-22 01:22:36 +0500

    Final_task_v.3

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -2,8 +2,8 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-string[] array1 = new string[4] {"Hello","2","world",":-)"};
-//string[] array2 = new string[4] {"1234","1567","-2","computer science"};
+//string[] array1 = new string[4] {"Hello","2","world",":-)"};
+string[] array1 = new string[4] {"1234","1567","-2","computer science"};
 //string[] array3 = new string[3] {"Russia","Denmark","Kazan"};
 string[] array2 = new string[array1.Length];
 
